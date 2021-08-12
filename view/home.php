79d6af9551b0cf0de54062d1285c70cbc9fc7ab9
DevMobydyg <desarrollo@mobydyg.com>
2021-08-11 22:06:02 -0600

    update

--- view/home.php
+++ view/home.php
@@ -1,1 +1,14 @@
-<h6>Home</h6>
+<div class="container">
+  <form method="POST" action="">
+    <div class="form-group">
+      <div class="row">
+        <div class="col">
+          <input type="filter" class="form-control" id="exampleInputFilter" aria-describedby="filterSearch" placeholder="Buscar">
+        </div>
+        <div class="col">
+          <button type="submit" class="btn btn-primary">Buscar</button>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
